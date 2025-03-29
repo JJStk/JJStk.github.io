7097380c89939626b110bf1b5eaeed6d20c2c8f3
JJStk <james.story@malad.us>
2025-03-28 21:27:06 -0600

    Update!
    Better notes, and the code has new functions (that were already in the stack and queue functions).

--- Back in my day LinkedList/DubLinkList.java
+++ Back in my day LinkedList/DubLinkList.java
@@ -1,3 +1,5 @@
+import org.w3c.dom.Node;
+
 class DubLinkList {
     
     private Node head;
@@ -10,6 +12,7 @@ class DubLinkList {
 
     }
 
+    // The queuer. One below (behind) the other.
     public void apply(int data) {
 
         Node bucket = new Node(data);
@@ -23,6 +26,8 @@ class DubLinkList {
         }
     }
 
+
+    // The stacker. One atop the other.
     public void preapply(int data) {
 
         Node bucket = new Node(data);
@@ -36,6 +41,7 @@ class DubLinkList {
         }
     }
 
+    // Erases nodes, it vaporises any and all contents in the bucket.
     public void eraser(int data) {
 
         Node current = head;
@@ -59,6 +65,8 @@ class DubLinkList {
         }
     }
 
+
+    // Reads the stack in the intended way, you're staring at a bucket.
     public void Forward() {
         Node current = head;
         while (current != null) {
@@ -68,6 +76,7 @@ class DubLinkList {
         System.out.println("End");
     }
 
+    // Reverses the stack, flips the bucket... somehow.
     public void Reverse() {
         Node current = tail;
         while (current != null) {
@@ -76,5 +85,49 @@ class DubLinkList {
         }
         System.out.println("End");
     }
+
+    // Everything above this line is my previous code which works as half the functions of a stack and queue.
+    // Those functions are the addition parts, but I had none of the removal. But not anymore...
+
+    
+    public int Ladle() {  // Stack popper
+        if (head == null) {
+            throw new RuntimeException("Fill the bucket, then try again.");
+        }
+        int value = head.data;
+        head = head.next;
+        if (head != null) {
+            head.prev = null;
+        } else {
+            tail = null;
+        }
+        return value;
+    }
+
+    public int GetOuttaLine() {  // Dequeuer
+        if (head == null) {
+            throw new RuntimeException("Line's empty");
+        }
+        int value = head.data;
+        head = head.next;
+        if (head != null) {
+            head.prev = null;
+        } else {
+            tail = null;
+        }
+        return value;
+    }
+
+    public int peer() {  // Peek
+        if (head == null) {
+            throw new RuntimeException("The bucket is empty, you didn't fill it.");
+        }
+        return head.data;
+    }
+
+
+
+
+
 
 }
--- Back in my day LinkedList/Main.java
+++ Back in my day LinkedList/Main.java
@@ -15,8 +15,19 @@ public class Main {
         System.out.println("Reverse! Reverse!:");
         dll.Reverse();
 
-        // You can delete and add new nodes as well!
-        // Just remember to remove the double slashes to use the premade code!
+        //           Commands:
+        // 'apply' is queue, 'preapply' is stack.
+        // Delete anything with 'eraser', just remember what you want to erase!
+        // 'peer' lets you see the #1 spot in the list.
+        // 'Ladle' pulls the #1 spot a.k.a. the toppa da stack.
+        // 'GetOuttaLine' removes the last queued, or the bottom of the stack.
+
+
+
+
+
+
+        // Remember to remove the double slashes to use the premade code!
 
         // dll.preapply(5);
         // System.out.println("5 is your new #1!");
