f6e1bc65e13f2ee351cfce79c4d717853f744f9b
JJStk <james.story@malad.us>
2025-04-04 18:33:39 -0600

    These are from another repository and are not mine
    They are to demonstrate how to fix bugs

---
+++ Fixed errors/error2.java
@@ -0,0 +1,40 @@
+/*
+Error filled file created by
+Kimg@techtrepacademy.com
+for my java class to learn troubleshooting on
+*/
+public class error2 { // gotta be the name of the file
+
+    public static void main(String[] args){ //Missed this... had me scratching my head for a bit there.
+
+    System.out.println("Hello world"); // I originally rewrote this line to fix the problem when the solution was to add the above code.
+
+    System.out.println("am I doing this correctly");
+
+    for (int i=0; i<10; i++){
+        System.out.println("the number i is "+i); // lm to ln
+    }
+
+    for (int i=0; i>10; i++){ // 
+        System.out.println("the new number i is "+i);
+
+        // if the i needs value, you can always put it inside the closed loop
+        if (i==0){ 
+            System.out.print("i is equal to zero");
+
+        }else {
+            System.out.print("i is not equal to zero");
+
+        }
+
+    }
+
+
+
+    
+    //what other errors can I put into a source code file?
+    // You could havse had a String with an int value and vice versa.
+
+}
+
+}
