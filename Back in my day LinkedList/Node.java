6f16330fc9fb9c6e908b786789fe92b3836ec1f4
JJStk <james.story@malad.us>
2025-05-07 19:45:39 -0600

    Update

--- Back in my day LinkedList/Node.java
+++ Back in my day LinkedList/Node.java
@@ -1,14 +1,14 @@
 class Node {
     
-    int data;
-    Node next;
-    Node prev;
+    int data; // Makes a place to hold data
+    Node next; // A string that will make it so that I can make a chain of data sets in one direction
+    Node prev; // A string that will make it so that I can make a chain of data sets in the other direction
 
     public Node(int data) {
 
-        this.data = data;
-        this.next = null;
-        this.prev = null;
+        this.data = data; // making the held data a string
+        this.next = null; // making so that you can put data attached to the first data set
+        this.prev = null; // making so that you can put data attached to the first data set, but the other direction
 
     }
 }
